b909a604009e2f8571391340261124071be4946d
Ana Barros <up201806593@fe.up.pt>
2022-01-04 18:20:10 +0000

    WIP: adde neighbour

--- src/proj2/src/main/java/main/network/neighbour/Host.java
+++ src/proj2/src/main/java/main/network/neighbour/Host.java
@@ -3,22 +3,24 @@ package main.network.neighbour;
 import java.net.InetAddress;
 import java.util.Objects;
 
-public class Host {
+public class Host implements Comparable<Host>{
     private final InetAddress address;
     private final String port;
     private final int capacity;
     // needed to add as neighbor
     private final String username;
+    private int degree;
 
-    public Host(String username, InetAddress address, String port, int capacity) {
+    public Host(String username, InetAddress address, String port, int capacity, int degree) {
         this.address = address;
         this.port = port;
         this.capacity = capacity;
         this.username = username;
+        this.degree = degree;
     }
 
     public Host(Host host) {
-        this(host.username, host.address, host.port, host.capacity);
+        this(host.username, host.address, host.port, host.capacity, host.degree);
     }
     @Override
     public boolean equals(Object o) {
@@ -41,5 +43,16 @@ public class Host {
 
     public int getCapacity() {
         return capacity;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    @Override
+    public int compareTo(Host host) {
+        if (capacity == host.capacity)
+            return Integer.compare(host.degree, degree);
+        return Integer.compare(host.capacity, capacity);
     }
 }
--- src/proj2/src/main/java/main/network/neighbour/Neighbour.java
+++ src/proj2/src/main/java/main/network/neighbour/Neighbour.java
@@ -1,21 +1,19 @@
 package main.network.neighbour;
 
-import main.timelines.Timeline;
-
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Neighbour extends Host{
-    private final List<Timeline> timelines;
+    private final List<String> stored_timelines;
 
-    public Neighbour(String username, InetAddress address, String port, int capacity, List<Timeline> timelines) {
-        super(username, address, port, capacity);
-        this.timelines = timelines;
+    public Neighbour(String username, InetAddress address, String port, int capacity, int degree) {
+        super(username, address, port, capacity, degree);
+        this.stored_timelines = new ArrayList<>();
     }
 
     public Neighbour (Host host) {
         super(host);
-        this.timelines = new ArrayList<>();
+        this.stored_timelines = new ArrayList<>();
     }
 }
