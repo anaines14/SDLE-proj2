b8a9b80f9ad670408ecec1293c740cca2445fe0d
Ana Barros <up201806593@fe.up.pt>
2022-01-05 18:43:27 +0000

    Add: QueryMessage

--- src/proj2/src/main/java/main/network/message/QueryMessage.java
+++ src/proj2/src/main/java/main/network/message/QueryMessage.java
@@ -2,7 +2,7 @@ package main.network.message;
 
 import main.network.PeerInfo;
 
-public class QueryMessage extends Message {
+public class QueryMessage extends MessageRequest {
     private final String wantedTimeline;
 
     public QueryMessage(String wantedTimeline, PeerInfo peerInfo) {
